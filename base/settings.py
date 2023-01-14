800b118eef7d8f615b1ace6fb4e74687ea3f6c2a
Kolawole Daniel <kolawolecoke@gmail.com>
2023-01-14 10:02:55 +0100

    removed leaked database info

--- base/settings.py
+++ base/settings.py
@@ -112,11 +112,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'yOOIJ7xIUwyCUvVqdBAq',
-        'HOST': 'containers-us-west-85.railway.app',
-        'PORT': '6298',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
